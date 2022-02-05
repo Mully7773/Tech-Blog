151358aadeab7fc891a9e03df1da7f9a8506f8ec
Nick M <mully7773@gmail.com>
2022-02-05 13:26:42 -0600

    Added user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,70 @@
 const router = require('express').Router();
+const { User } = require('../../models');
+
+//User create
+router.post('/', async (req, res) => {
+  try {
+    //   console.log(req.body)
+    const userData = await User.create({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 
+//User login
+router.post('/login', async (req, res) => {
+    try {
+      const userData = await User.findOne({ where: { email: req.body.username } });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect username or password, please try again' });
+        return;
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect username or password, please try again' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+  
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+//User logout
+  router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
 
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/',  (req, res) => {
     res.render('homepage')
