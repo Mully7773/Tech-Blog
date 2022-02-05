67c4d9ecfb1c3a0503bedd1eadd57ca7411c5d88
Nick M <mully7773@gmail.com>
2022-02-05 12:03:08 -0600

    homepage is displaying--more route initialization

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,1 +1,10 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
 
+router.get('/',  (req, res) => {
+    res.render('homepage')
+  });
+
+
+
+  module.exports = router;
