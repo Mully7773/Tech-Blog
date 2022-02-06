f3c03156713b5c118a1f1fcbee832678c62e9bd4
Nick M <mully7773@gmail.com>
2022-02-05 19:22:17 -0600

    Added new post handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,24 +1,67 @@
 const router = require('express').Router();
+const { DESCRIBE } = require('sequelize/types/lib/query-types');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/',  (req, res) => {
-    res.render('homepage')
+// router.get('/', async (req, res) => {
+//     try {
+//         const posts = await Post.findAll({
+//         raw: true,
+//         order: [["id", "DESC"]],
+//         });
+//         const postData = {
+//             posts
+//         }
+//         res.render('homepage', postData);  
+//     } catch (err) {
+//                 console.log("There's an error:", err);
+//            }
+    
+//   });
+
+  router.get('/', async (req, res) => {
+    try {
+      // Get all posts and JOIN with user data
+      const postData = await Post.findAll({
+        raw: true,
+        order: [["id", "DESC"]],
+        include: 
+          {
+            model: User,
+            attributes: ['username']
+          },
+      });
+  
+      // Serialize data so the template can read it
+      const posts = posts.map((post) => post.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('all-posts', { 
+        posts, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
   });
 
 
   
 router.get('/login',  (req, res) => {
+    // if(req.session.loggedIn) {
+    //     res.redirect('/');
+    //     return;
+    //}
     res.render('login')
   });
 
-
-
-  
-router.get('/signup',  (req, res) => {
+router.get('/signup', (req, res) => {
+    // if(req.session.loggedIn) {
+    //     res.redirect('/');
+    //     return;
+    // }
     res.render('signup')
-  });
-
+});
 
 
   module.exports = router;
